2efeb72bfdffd89a06916929ec2c55085ef8aa7f
dengbowen <dengbowen17@outlook.com>
2021-11-30 21:52:51 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-tmux kill-session -t django
-tmux new-session -d -s django
-tmux send-keys "cd ~/project/DialogueRobot" C-m
-tmux send-keys "git pull" C-m
-tmux send-keys "cd ~/project/DialogueRobot/DiaRobot" C-m
-tmux send-keys "sudo ~/.conda/envs/robot/bin/python manage.py makemigrations" C-m
-tmux send-keys "sudo ~/.conda/envs/robot/bin/python manage.py migrate" C-m
-tmux send-keys "sudo ~/.conda/envs/robot/bin/python manage.py runserver 0.0.0.0:80" C-m
-# port=8000
-# pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{print $1}')
-# if [ -n "$pid" ]; then
-#     kill -9 $pid;
-# fi
+# tmux kill-session -t django
+# tmux new-session -d -s django
+# tmux send-keys "cd ~/project/DialogueRobot" C-m
+# tmux send-keys "git pull" C-m
+# tmux send-keys "cd ~/project/DialogueRobot/DiaRobot" C-m
+# tmux send-keys "sudo ~/.conda/envs/robot/bin/python manage.py makemigrations" C-m
+# tmux send-keys "sudo ~/.conda/envs/robot/bin/python manage.py migrate" C-m
+# tmux send-keys "sudo ~/.conda/envs/robot/bin/python manage.py runserver 0.0.0.0:80" C-m
 
-# source /usr/local/anaconda3/etc/profile.d/conda.sh
-# conda activate robot
-# git -C ~/DialogueRobot pull
-# nohup python ~/DialogueRobot/DiaRobot/manage.py runserver 0.0.0.0:8000 >/dev/null 2>&1 &
+port=80
+pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{print $1}')
+if [ -n "$pid" ]; then
+    kill -9 $pid;
+fi
+
+git -C ~/project/DialogueRobot pull
+sudo ~/.conda/envs/robot/bin/python ~/project/DialogueRobot/DiaRobot/manage.py runserver 0.0.0.0:80
