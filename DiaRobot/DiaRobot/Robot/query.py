2793a328b0fabdf4369132aae18db13d6b415d64
kangshuaifunotkangshifu <1446540925@qq.com>
2021-12-05 15:28:13 +0800

    test weather1

--- DiaRobot/DiaRobot/Robot/query.py
+++ DiaRobot/DiaRobot/Robot/query.py
@@ -1,5 +1,61 @@
 #todo
 #根据用户输入返回查询结果
 #如果必要信息不足需要引导用户继续输入
-def Query():
-    pass
+import requests
+from bs4 import BeautifulSoup
+from wechatpy import parse_message, create_reply
+from django.http import HttpResponse
+
+def crawlweather():  # 爬取天气数据
+    url = 'http://www.weather.com.cn/weather/101010100.shtml'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
+
+    resp = requests.get(url, headers)
+    resp.encoding = 'utf-8'
+    date = []
+    weather = []
+    temp_h = []
+    temp_l = []
+    wind = []
+    windlevel = []
+    bs = BeautifulSoup(resp.text, 'lxml')
+    datelab = bs.select('ul.clearfix>li.sky>h1')
+    for each in datelab:
+        date.append(each.text)
+    wealab = bs.select('ul.clearfix>li.sky>p.wea')
+    for each in wealab:
+        weather.append(each.text)
+    tem_high_lab = bs.select('ul.clearfix>li.sky>p.tem>span')
+    for each in tem_high_lab:
+        temp_h.append(each.text)
+    tem_low_lab = bs.select('ul.clearfix>li.sky>p.tem>i')
+    for each in tem_low_lab:
+        temp_l.append(each.text)
+    winlab = bs.select('ul.clearfix>li.sky>p.win>em>span')
+    for each in winlab:
+        wind.append(each.get('title'))
+    windlevlab = bs.select('ul.clearfix>li.sky>p.win>i')
+    for each in windlevlab:
+        windlevel.append(each.text)
+    return date, weather, temp_h, temp_l, wind, windlevel  # 返回存储数据的五个数组
+
+def queryweather(num, msg):
+    daycount=0
+    if num == "三" or num == "3":
+        daycount = 3
+    elif num == "七" or num == "7":
+        daycount = 7
+    else:
+        daycount=1
+    date, weather, temp_h, temp_l, wind, windlevel = crawlweather()
+    res = ""
+    for i in range(0, daycount):
+        res = res + date[i] + '  '+ weather[i] + '  '+ temp_h[i] + "/" + temp_l[i] + '  '+wind[i] + windlevel[i]+'\n'
+    reply = create_reply(res, msg)
+    response = HttpResponse(reply.render(), content_type="application/xml")
+    return response
+
+
+def Query(msg):
+    if msg.content =='查询天气':
+        return queryweather("7", msg)
--- DiaRobot/DiaRobot/view.py
+++ DiaRobot/DiaRobot/view.py
@@ -6,6 +6,7 @@ from wechatpy.utils import check_signature
 from wechatpy.exceptions import InvalidSignatureException
 from wechatpy.replies import BaseReply
 
+import DiaRobot.Robot.query
 from DiaRobot.Robot.nlp import Preprocess
 from DiaRobot.Robot.nlp import Status
 
@@ -13,6 +14,7 @@ from DiaRobot.Robot.navigation import Navigation
 from DiaRobot.Robot.query import Query
 from DiaRobot.Robot.reserve import Reserve
 from DiaRobot.Robot.recommend import Recommend
+from Robot.query import *
 
 TOKEN = 'dengzhouzhang'
 
@@ -34,6 +36,9 @@ def handle_wx(request):
 
     # POST方式用于接受和返回请求
     else:
+        msg = parse_message(request.body)
+        response = DiaRobot.Robot.query.Query(msg)
+        return response
         #begin reply生成示例
         # reply = None
         # msg = parse_message(request.body)
@@ -59,30 +64,30 @@ def handle_wx(request):
         #2.根据nlp模块状态对应的分支模块，导航、预约、查询、推荐
         #3.生成回答并返回
 
-        msg = parse_message(request.body)
+        # msg = parse_message(request.body)
+        #
+        # pre = Preprocess(msg.content)
+        # pre.MsgProcess()
+        #
+        # # 导航模块
+        # if pre.status == Status.navi:
+        #     pass
+        #
+        # # 查询模块
+        # elif pre.status == Status.que:
+        #     pass
+        #
+        # # 推荐模块
+        # elif pre.status == Status.recom:
+        #     pass
+        #
+        # # 预约模块
+        # elif pre.status == Status.reser:
+        #     pass
+        #
+        # # 异常处理模块
+        # elif pre.status == Status.unclear:
+        #     reply = create_reply('暂不支持您的需求', msg)
 
-        pre = Preprocess(msg.content)
-        pre.MsgProcess()
-
-        # 导航模块
-        if pre.status == Status.navi:
-            pass
-
-        # 查询模块
-        elif pre.status == Status.que:
-            pass
-
-        # 推荐模块
-        elif pre.status == Status.recom:
-            pass
-
-        # 预约模块
-        elif pre.status == Status.reser:
-            pass
-
-        # 异常处理模块
-        elif pre.status == Status.unclear:
-            reply = create_reply('暂不支持您的需求', msg)
-
-        response = HttpResponse(reply.render(), content_type="application/xml")
-        return response
+        # response = HttpResponse(reply.render(), content_type="application/xml")
+        # return response
